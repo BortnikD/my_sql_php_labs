5795555bb22c7e0bfc9392b2af19096e7907f2d4
BortnikD <dima100421@gmail.com>
2026-04-09 18:57:09 +0300

    feat(audit, cors): Добавлены поддерживаемые http методы, измен allowed host для фронта

--- audit/src/config/Cors.php
+++ audit/src/config/Cors.php
@@ -3,8 +3,8 @@
 class Cors {
 
     public static function handle(): void {
-        header('Access-Control-Allow-Origin: http://localhost:5173');
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Origin: http://localhost:3000');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type');
         header('Content-Type: application/json; charset=utf-8');
 
